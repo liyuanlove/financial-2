e2a4100b0417e1d8c78d8214d43c50d6eea183ce
liangjh <liangjh@iaskdoc.com>
2018-07-20 22:47:01 +0800

    feat:git仓库初始化

---
+++ entity/src/main/java/com/ljh/entity/Product.java
@@ -0,0 +1,15 @@
+package com.ljh.entity;
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+
+/**
+ * @author liangjh
+ * @create 2018-07-20 22:21
+ **/
+@Data
+@Entity
+public class Product {
+
+}
---
+++ entity/src/main/java/com/ljh/enums/OrderStatusEnum.java
@@ -0,0 +1,9 @@
+package com.ljh.enums;
+
+/**
+ * @author liangjh
+ * @create 2018-07-20 22:26
+ **/
+public enum OrderStatusEnum {
+
+}
