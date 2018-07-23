9f98271e96bdfa37dbac8d97bff51ddb8c0952e2
liangjh <liangjh@iaskdoc.com>
2018-07-23 20:29:50 +0800

    style

--- src/main/java/com/ljh/financial/enums/OrderTypeEnum.java
+++ src/main/java/com/ljh/financial/enums/OrderTypeEnum.java
@@ -4,6 +4,6 @@ package com.ljh.financial.enums;
  * @author liangjh
  * @create 2018-07-20 22:25
  **/
-public enum  OrderTypeEnum {
+public enum OrderTypeEnum {
 
 }
--- src/main/java/com/ljh/financial/enums/ProductStatusEnum.java
+++ src/main/java/com/ljh/financial/enums/ProductStatusEnum.java
@@ -4,6 +4,6 @@ package com.ljh.financial.enums;
  * @author liangjh
  * @create 2018-07-20 22:24
  **/
-public enum  ProductStatusEnum {
+public enum ProductStatusEnum {
 
 }
