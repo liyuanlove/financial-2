1f59bdf53c00d5e389a3d8575a1b8d62be56b3b3
liangjh <liangjh@iaskdoc.com>
2018-07-23 22:25:04 +0800

    feat:修复idea报错

--- src/main/java/com/ljh/financial/mapper/CommonMapper.java
+++ src/main/java/com/ljh/financial/mapper/CommonMapper.java
@@ -1,6 +1,7 @@
 package com.ljh.financial.mapper;
 
 
+import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 import tk.mybatis.mapper.common.MySqlMapper;
 
@@ -8,6 +9,7 @@ import tk.mybatis.mapper.common.MySqlMapper;
  * @author liangjh
  * @create 2018-07-22 19:07
  **/
+@Repository
 public interface CommonMapper<T> extends Mapper<T>, MySqlMapper<T> {
 
 }
--- src/main/java/com/ljh/financial/mapper/ProductMapper.java
+++ src/main/java/com/ljh/financial/mapper/ProductMapper.java
@@ -1,6 +1,8 @@
 package com.ljh.financial.mapper;
 
 import com.ljh.financial.entity.Product;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductMapper extends CommonMapper<Product> {
 }
